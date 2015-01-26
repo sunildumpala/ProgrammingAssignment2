86e6898899f4de94f74e27e8c3782b419516d879
Sunil Dumpala <sdumpala@MacBook-Air.local>
2015-01-25 16:55:26 -0800

    My additions final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeCacheMatrix function does the following
+## set the value of the matrix
+## get the value of the matrix
+## set the inverse of the matrix
+## get the inverse of the matrix
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 	i <- NULL
@@ -18,11 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## cacheSolve checks that the matrix is not null
+## and it is same as the original matrix used in makeCacheMatrix
+## If both conditions are valid then it checks if the inverse is cached and it is not null
+## Returns the inverse from cache if available, or else saves the inverse in cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i <- x$getInverse()
-        if(!is.null(i))
+        if(!is.null(i) && !(x == x$get()))
         {
         	message("getting cached data")
         	return(i)
